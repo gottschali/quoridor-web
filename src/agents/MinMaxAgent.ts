1e53ac6f92203407d6196b48306ff9b4114c9876
Gottschali <aligottschall@protonmail.com>
2022-07-04 11:14:16 +0200

    Experiment with WallsHeuristic

--- src/agents/MinMaxAgent.ts
+++ src/agents/MinMaxAgent.ts
@@ -1,33 +1,38 @@
 import { Move } from "../quoridor/Move";
 import { Player } from "../quoridor/Player";
 import { State } from "../quoridor/State";
+import { wallsHeuristic } from "./wallsHeuristic";
 
-function evaluateState(state: State, evalPlayer: Player) {
+export function evaluateState(state: State, evalPlayer: Player) {
     // Perform BFS for both pawns to get distance to finishline
     const d1 = state.BFS(state.pawnPositions[evalPlayer], evalPlayer);
     const otherPlayer = evalPlayer === Player.white ? Player.black : Player.white;
     const d2 = state.BFS(state.pawnPositions[otherPlayer], otherPlayer);
     // factor in the walls available
-    return (10-d1) + d2;
+    return (20-d1) + d2 + Math.random();
 }
 
 let nodes = 0;
 
-function minMax(state: State, maxPlayer: Player, depth: number) {
+export type Heuristic = (state: State)=>Move[];
+
+function minMax(state: State, maxPlayer: Player, depth: number, heuristic: Heuristic) {
     nodes++;
     if (state.isGameOver()) {
-        return state.winner() === maxPlayer ? 42 : -42;
+        return state.winner() === maxPlayer ? Number.POSITIVE_INFINITY : Number.NEGATIVE_INFINITY;
     }
     if (depth <= 0) {
         // return board evaluation
         return evaluateState(state, maxPlayer);
     }
 
+    const cands = heuristic(state);
+    console.log(`Heuristic: ${cands.length}`);
     console.log(`Recursively exploring all ${state.legalMoves.length} moves with depth ${depth}`);
     if (state.currentPlayer === maxPlayer) {
         let t = Number.NEGATIVE_INFINITY;
-        for (const child of state.legalMoves) {
-            const value = minMax(state.makeMove(child), maxPlayer, depth - 1);
+        for (const child of cands) {
+            const value = minMax(state.makeMove(child), maxPlayer, depth - 1, heuristic);
             if (value >= t) {
                 t = value;
             }
@@ -35,8 +40,8 @@ function minMax(state: State, maxPlayer: Player, depth: number) {
         return t;
     } else {
         let t = Number.POSITIVE_INFINITY;
-        for (const child of state.legalMoves) {
-            const value = minMax(state.makeMove(child), maxPlayer, depth - 1);
+        for (const child of cands) {
+            const value = minMax(state.makeMove(child), maxPlayer, depth - 1, heuristic);
             if (value <= t) {
                 t = value;
             }
@@ -46,14 +51,17 @@ function minMax(state: State, maxPlayer: Player, depth: number) {
 }
 
 
-export function MinMaxAgent(depth=5) {
+export function MinMaxAgent(depth=5, heuristic: Heuristic=(state)=>state.legalMoves) {
     function minMaxWrapper(state: State): Move {
         nodes = 0;
         let t = Number.NEGATIVE_INFINITY;
         let tempMove = state.legalMoves[0];
         console.log(`Exploring all ${state.legalMoves.length} moves with a depth of ${depth}`)
-        for (const child of state.legalMoves) {
-            const value = minMax(state.makeMove(child), state.currentPlayer, depth-1);
+
+        const cands = heuristic(state);
+        console.log(`Heuristic: ${cands.length}`);
+        for (const child of cands) {
+            const value = minMax(state.makeMove(child), state.currentPlayer, depth-1, heuristic);
             if (value >= t) {
                 t = value;
                 tempMove = child;
---
+++ src/agents/wallsHeuristic.ts
@@ -0,0 +1,46 @@
+import { Coord } from "../quoridor/Coord";
+import { Move, Orientation, WallMove } from "../quoridor/Move";
+import { State } from "../quoridor/State";
+
+export function wallsHeuristic(state: State): Move[] {
+    const candidates: Move[] = [];
+    for (let move of state.legalMoves) {
+        if ('source' in move) candidates.push(move);
+        else {
+            const {square, orientation} = move as WallMove;
+            const other = square.add(orientation === Orientation.Vertical ? new Coord(2, 0) : new Coord(0, 2));
+
+            if (square.neighbours(1).some(c => c.equals(state.pawnPositions[state.currentPlayer]))) {
+                candidates.push(move);
+            } else if (square.neighbours(1).some(c => c.equals(state.pawnPositions[state.opponent]))) {
+                candidates.push(move);
+            }
+            if (square.neighbours(3).some(c => c.equals(state.pawnPositions[state.currentPlayer]))) {
+                candidates.push(move);
+            } else if (square.neighbours(3).some(c => c.equals(state.pawnPositions[state.opponent]))) {
+                candidates.push(move);
+            }
+            else {
+                const x = new Set<Coord>();
+
+                x.add(other.add(new Coord(1, 1)))
+                x.add(other.add(new Coord(-1, -1)))
+                x.add(other.add(new Coord(-1, 1)))
+                x.add(other.add(new Coord(1, -1)))
+
+                x.add(square.add(new Coord(1, 1)))
+                x.add(square.add(new Coord(-1, -1)))
+                x.add(square.add(new Coord(-1, 1)))
+                x.add(square.add(new Coord(1, -1)))
+
+                // x.forEach(c => {
+                //     if (state.board[c.row][c.column]) {
+                //         candidates.push(move);
+                //     }
+                // })
+            }
+
+        }
+    }
+    return candidates;
+}
