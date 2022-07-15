85fb7817674c79575b95516027375fedc97965d5
Gottschali <aligottschall@protonmail.com>
2022-07-15 12:03:06 +0200

    Outsource minmaxWrapper

--- src/agents/MinMaxAgent.ts
+++ src/agents/MinMaxAgent.ts
@@ -1,78 +1,16 @@
-import { Notation } from "../quoridor/Notation";
 import { State } from "../quoridor/State";
  // eslint-disable-next-line import/no-webpack-loader-syntax
 import Worker from "worker-loader!./MinMaxWorker";
 import { MachineAgent } from "./Agent";
-import { shuffleArray } from "./utils";
+import { minMaxWrapper } from "./minMax";
+import { shortestPathMove } from "./ShortestPathAgent";
 
 export let nodes = 0;
 
 let workers = new Array(navigator.hardwareConcurrency || 4).fill(null)
     .map(()=> new Worker());
 
-export function MinMaxAgent(depth=5): MachineAgent {
-
-
-    async function minMaxWrapper(state: State): Promise<Notation> {
-        let start = performance.now();
-        let alpha = Number.NEGATIVE_INFINITY;
-        let beta = Number.POSITIVE_INFINITY;
-        const kids = [...state.children.entries()];
-        shuffleArray(kids);
-        let t = Number.NEGATIVE_INFINITY;
-        let tempMove = "";
-        const promises = [];
-        const n = kids.length;
-        const workloads = [];
-        // const workers = [new Worker(), new Worker(), new Worker(), new Worker()];
-        for (let i=0;i<workers.length-1;i++) {
-            workloads.push(kids.splice(0, n / workers.length));
-        }
-        workloads.push(kids);
-
-        for (let i=0;i<workers.length;i++) {
-            const worker = workers[i];
-            const load = workloads[i];
-            const data: [Notation, number][] = [];
-            const p = new Promise<[Notation, number][]>(function(resolve) {
-                // Reusing workers didnt work because onmessage was overwritten and promises were not resolved
-                worker.onmessage = function(e: MessageEvent) {
-                    data.push(e.data);
-                    if (data.length === load.length) {
-                        resolve(data);
-                    }
-                }
-                for (const [move, child] of load) {
-                    const stateNotation = child.toNotation();
-                    worker.postMessage([move, stateNotation, state.settings, alpha, beta, state.currentPlayer, depth-1, false]);
-                }
-            })
-            if (load.length) promises.push(p);
-        }
-
-
-        await Promise.all(promises)
-        .then((data) => {
-            console.log("All promises resolved, merging results", data);
-            for (const part of data) {
-                for (const d of part) {
-                    const move = d[0];
-                    const value = d[1];
-                    if (value >= t) {
-                        t = value;
-                        tempMove = move;
-                    }
-                    alpha = Math.max(alpha, t)
-                    // Problem: can't break out of the loop because we are in a callback function
-                    // if (t >= beta) {
-                    //     break loop;
-                    // }
-                }
-            }
-        });
-        console.log(`Executed alpha-beta-min-max in ${performance.now() - start} ms`)
-        return tempMove;
-    }
+export function MinMaxAgent(depth=2): MachineAgent {
 
     function terminate() {
         for (const worker of workers) {
@@ -92,11 +30,18 @@ export function MinMaxAgent(depth=5): MachineAgent {
     // worker.onmessage = function(e) {
     //     console.log('Message received from worker', e.data);
     // }
+    function getMove(state: State) {
+        if (state.wallsAvailable[state.currentPlayer] === 0) {
+            return shortestPathMove(state);
+        } else {
+            return minMaxWrapper(state, depth, workers)
+        }
+    }
 
     return {
         isMachine: true,
         name: 'MinMaxAgent',
-        getMove: minMaxWrapper,
+        getMove: getMove,
         terminate,
     }
 }
--- src/agents/minMax.ts
+++ src/agents/minMax.ts
@@ -1,11 +1,80 @@
+import { Notation } from "../quoridor/Notation";
 import { Player } from "../quoridor/Player";
 import { State } from "../quoridor/State";
 import { evaluateState } from "./evaluateState";
 import { shuffleArray } from "./utils";
 
+
+type minMaxArgs = {
+  state: State;
+  alpha: number;
+  beta: number;
+  maxPlayer: Player;
+  depth: number;
+  maximizing: boolean;
 }
 
-export function minMax({ state, alpha, beta, maxPlayer, depth, maximizing}: { state: State; alpha: number; beta: number; maxPlayer: Player; depth: number; maximizing: boolean; }): number {
+
+export async function minMaxWrapper(state: State, depth: number, workers: Worker[]): Promise<Notation> {
+  let start = performance.now();
+  let alpha = Number.NEGATIVE_INFINITY;
+  let beta = Number.POSITIVE_INFINITY;
+  const kids = [...state.children.entries()];
+  shuffleArray(kids);
+  let t = Number.NEGATIVE_INFINITY;
+  let tempMove = "";
+  const promises = [];
+  const n = kids.length;
+  const workloads = [];
+  // const workers = [new Worker(), new Worker(), new Worker(), new Worker()];
+  for (let i = 0; i < workers.length - 1; i++) {
+    workloads.push(kids.splice(0, n / workers.length));
+  }
+  workloads.push(kids);
+
+  for (let i = 0; i < workers.length; i++) {
+    const worker = workers[i];
+    const load = workloads[i];
+    const data: [Notation, number][] = [];
+    const p = new Promise<[Notation, number][]>(function (resolve) {
+      // Reusing workers didnt work because onmessage was overwritten and promises were not resolved
+      worker.onmessage = function (e: MessageEvent) {
+        data.push(e.data);
+        if (data.length === load.length) {
+          resolve(data);
+        }
+      }
+      for (const [move, child] of load) {
+        const stateNotation = child.toNotation();
+        worker.postMessage([move, stateNotation, state.settings, alpha, beta, state.currentPlayer, depth - 1, false]);
+      }
+    })
+    if (load.length) promises.push(p);
+  }
+  await Promise.all(promises)
+    .then((data) => {
+      console.log("All promises resolved, merging results", data);
+      for (const part of data) {
+        for (const d of part) {
+          const move = d[0];
+          const value = d[1];
+          if (value >= t) {
+            t = value;
+            tempMove = move;
+          }
+          alpha = Math.max(alpha, t)
+          // Problem: can't break out of the loop because we are in a callback function
+          // if (t >= beta) {
+          //     break loop;
+          // }
+        }
+      }
+    });
+  console.log(`Executed alpha-beta-min-max in ${performance.now() - start} ms`)
+  return tempMove;
+}
+
+export function minMax({ state, alpha, beta, maxPlayer, depth, maximizing }: minMaxArgs): number {
   if (state.isGameOver()) {
     return state.winner() === maxPlayer ? Number.POSITIVE_INFINITY : Number.NEGATIVE_INFINITY;
   }
@@ -39,3 +108,4 @@ export function minMax({ state, alpha, beta, maxPlayer, depth, maximizing}: { st
     return value;
   }
 }
+
