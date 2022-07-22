c501c414df7d333e8af8ba61e1be51be062ebfeb
Gottschali <aligottschall@protonmail.com>
2022-07-22 09:29:16 +0200

    Less badges
    On mobile they were too large for the viewport

--- src/ui/GameInformation.tsx
+++ src/ui/GameInformation.tsx
@@ -17,25 +17,16 @@ export function GameInformation({currentAgent, game}: Props) {
                 <HStack >
                     {game.state.isGameOver()
                         ? <Badge fontSize='ml' colorScheme={currentAgent.isMachine ? 'green' : 'red'}> Winner: {winnerIcon}</Badge>
-                        : <Badge fontSize='ml'> Winner: undecided </Badge>
+                        : <Badge fontSize='ml' variant={game.state.currentPlayer === Player.white ? 'outline' : 'solid'} colorScheme='blackAlpha'> turn: {game.turn} </Badge>
                     }
-                    <Badge fontSize='ml' variant={game.state.currentPlayer === Player.white ? 'outline' : 'solid'} colorScheme='blackAlpha'>
-                        turn: {game.turn}
-                    </Badge>
-                    <Badge fontSize='ml'  variant='outline' colorScheme='blackAlpha'>
-                        <Text color='black'>
-                        walls ○ : {game.state.wallsAvailable[Player.white]}
-                        </Text>
-                    </Badge>
-                    <Badge fontSize='ml' variant='solid' colorScheme='blackAlpha'>
-                        walls ●: {game.state.wallsAvailable[Player.black]}
+                    <Badge fontSize='ml'>
+                        walls ●: {game.state.wallsAvailable[Player.black]} ○ : {game.state.wallsAvailable[Player.white]}
                     </Badge>
                     <Badge fontSize='ml'>Player: {currentAgent.name}
                         {currentIcon}
                     </Badge>
                     <Badge fontSize='ml'>
                         {currentAgent.isMachine && !game.state.isGameOver() && <CircularProgress isIndeterminate size='1em'/>}
-                        {!currentAgent.isMachine && 'Your turn'}
                     </Badge>
                 </HStack>
             </Center>
